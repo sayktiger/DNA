d82ee7d7cc63763568a3b256eec8d3f6a798568b
Konstantin <sayk.tiger09@gmail.com>
2022-09-24 12:18:42 +0300

    adaptive 480px ready + mobile-accordion

--- src/js/script.js
+++ src/js/script.js
@@ -132,4 +132,36 @@ document.addEventListener(`DOMContentLoaded`, () => {
             disableOnInteraction: true
           },
       });
+
+      //Аккардион на мобилке
+      const accordions = document.querySelectorAll(".accordion");
+        const openAccordion = (accordion) => {
+            const content = accordion.querySelector(".accordion__content");
+            accordion.classList.add("accordion__active");
+            content.style.maxHeight = content.scrollHeight + "px";
+        };
+
+        const closeAccordion = (accordion) => {
+            const content = accordion.querySelector(".accordion__content");
+            accordion.classList.remove("accordion__active");
+            content.style.maxHeight = null;
+        };
+
+        accordions.forEach((accordion) => {
+            const intro = accordion.querySelector(".accordion__intro");
+            const content = accordion.querySelector(".accordion__content");
+            if(accordion.classList.contains == `accordion__active`){
+                accordions.forEach((accordion) => closeAccordion(accordion));
+                openAccordion(accordion);
+            }
+            intro.onclick = () => {
+                if (content.style.maxHeight) {
+                    closeAccordion(accordion);
+                } else {
+                    accordions.forEach((accordion) => closeAccordion(accordion));
+                    openAccordion(accordion);
+                }
+            };
+        });
+
 });
